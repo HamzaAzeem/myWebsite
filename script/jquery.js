23e8334c9c3bd821448dfc726e3164a7c2451143
Hamza Azeem <hamzaazeem1@gmail.com>
2014-03-27 14:22:22 -0400

    Split stylesheets
    Fixed annoyances
    Index
    - Added random background changer
    - Re-positioned items slightly (changed z-indexes)
    - Removed Google Plus icon
    
    uWaterloo page
    - Changed font for "Back to top" button
    - Fixed anchor link problem with "First" link
    - Darkened texture background
    
    About Me page
    - Darkened texture background

--- script/jquery.js
+++ script/jquery.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
+$(function() {
+	//"Back to top" button animation
 	window.addEventListener('scroll', function() {
 		if(window.scrollY < 333) {
 			$('#btt').slideUp();
-
 		}
 		else {
 			$('#btt').slideDown();
 		}
 	}, false);
+
+	//Random background changer
+	$('#changeBG').on('click', function() {
+   		$('html').css('background-image', 'url(images/wall' + Math.floor((Math.random()*5)+1) + '.jpg)');
+  	});
 });
