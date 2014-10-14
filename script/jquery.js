b2fcc58e372997d6336c9d6b314a38ea7a25ba90
HamzaAzeem <hamzaazeem1@gmail.com>
2014-10-14 13:31:32 -0400

    Update

--- script/jquery.js
+++ script/jquery.js
@@ -21,29 +21,29 @@ $(function() {
 		}
 	}
 
+	//E-mail tooltip (experimental)
+	$("#mail").on('click', function () {
+		$('div.tooltip').toggle('fast');
+		$('.tooltip input').select();
+	});
+
+	$('#mobile-nav').on('click', function() {
+		$('#menu').toggleClass('mobi');
+		$('#mobile-nav').toggleClass('clicked');
+	});
+
 	$('html').dblclick(function() {
 		var bg =  'url(images/wall' + Math.floor((Math.random()*7)+1) + '.jpg)';
 		console.log("bg", bg);
 		localStorage.setItem("bg", bg);
 		loadBg();
-  	});
-
-  	//E-mail tooltip (experimental)
-  	$("#mail").on('click', function () {
-	    $('div.tooltip').toggle('fast');
-	    $('.tooltip input').select();
 	});
 
-  	$('#mobile-nav').on('click', function() {
-  		$('#menu').toggleClass('mobi');
-  		$('#mobile-nav').toggleClass('clicked');
-  	});
-
-  	$('h1').on('click', function() {
-  		console.log('clicked');
-  		$(this).next('section').toggleClass('visibility').not(':first');
-  	});
-  	
+	$('h1').on('click', function() {
+		console.log('clicked');
+		$(this).next('section').toggleClass('visibility').not(':first');
+	});
+	
 	loadBg();
 
 });
